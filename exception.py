ddbc033d9a835717a388c4187ab212ebaea1f337
riteshbirajdar7 <riteshbirajdarstudent@gmail.com>
2025-08-22 19:38:40 +0530

    Modified the comments in the code

--- exception.py
+++ exception.py
@@ -3,7 +3,7 @@ try:
 except:
     print("error occured") # this is gonna print
 else:
-    print("All correct") # it is going to occur either error is there or not
+    print("All correct") # it is going to occur if there is no error
 
 
 #ANother example with no errors
@@ -12,4 +12,4 @@ try:
 except:
     print("error occured") #it is not going to print as error is not there
 else:
-    print("All correct") # it is going to occur either there is error or not
+    print("All correct") # it is going to occur as there is no error
